83ba42d17af80fe25c0910dbb177d17efebe1f7e
Jacob Ko <tko003@ucr.edu>
2022-11-28 14:07:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.6jiio2q.mongodb.net/game_library?retryWrites=true&w=majority";
+const mongoDB = "mongodb url here";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
