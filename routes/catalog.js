85f30a641d6b8a0a6fc5cb52aac5e6e7f9d51295
Jacob Ko <tko003@ucr.edu>
2022-11-29 17:57:56 -0800

    Finished dummy routes for Items and Categories

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,16 +7,32 @@ const category_controller = require("../controllers/categoryController");
 
 /// Item ROUTES ///
 
-// GET catalog home page.
+// GET home page.
 router.get("/", item_controller.index);
 
 // GET request for list of all Items.
 router.get("/item", item_controller.item_list);
 
-// GET request for creating an item.
+// GET request for creating an Item.
 router.get("/item/create", item_controller.item_create_get);
 
-router.get("item/create")
+// POST request for creating an Item.
+router.post("/item/create", item_controller.item_create_post);
+
+// GET request for deleting an Item.
+router.get("/item/:id/delete", item_controller.item_delete_get);
+
+// POST request for deleting an Item.
+router.post("item/:id/delete", item_controller.item_delete_post);
+
+// GET request for updating an Item.
+router.get("/item/:id/update", item_controller.item_update_get);
+
+// POST request for updating an Item.
+router.post("/item/:id/update", item_controller.item_update_post);
+
+// GET request for detail page for a specific Item.
+router.get("/item/:id", item_controller.item_detail)
 
 
 /// Category ROUTES ///
@@ -24,5 +40,25 @@ router.get("item/create")
 // GET request for list of all Categories.
 router.get("/category", category_controller.category_list);
 
+// GET request for creating a Category.
+router.get("/category/create", category_controller.category_create_get);
+
+// POST request for creating a Category.
+router.post("/category/create", category_controller.category_create_post);
+
+// GET request for deleting a Category.
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// POST request for deleting a Category.
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// GET request for updating a Category.
+router.get("/category/:id/update", category_controller.category_update_get);
+
+// POST request for updating a Category.
+router.post("/category/:id/update", category_controller.category_update_post);
+
+// GET request for detail page for a specific Category.
+router.get("/category/:id", category_controller.category_detail);
 
 module.exports = router;
