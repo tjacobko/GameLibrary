bac11a2c6f19d243ea43732fc3bc1012965a3aaa
Jacob Ko <tko003@ucr.edu>
2022-12-04 23:55:15 -0800

    Created form for category_create get and post.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,7 @@ const Category = require('../models/category');
 const Item = require('../models/item');
 
 const async = require('async');
+const { body, validationResult } = require("express-validator");
 
 // Display list of all categories
 exports.category_list = (req, res, next) => {
@@ -53,14 +54,52 @@ exports.category_detail = (req, res, next) => {
 };
 
 // Display Category create form on GET.
-exports.category_create_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: Category create GET");
+exports.category_create_get = (req, res, next) => {
+    res.render("category_form", { title: "Create Category" });
 };
 
 // Handle Category create on POST.
-exports.category_create_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: Category create POST");
-};
+exports.category_create_post = [
+    // Validate and sanitize fields.
+    body("name", "Title must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Author must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+            res.render("category_form", {
+                title: "Create Category",
+                category: req.body,
+                errors: errors.array(),
+            });
+            return;
+        }
+        // Data from form is valid.
+
+        // Create a Category object with escaped and trimmed data.
+        const category = new Category({
+            name: req.body.name,
+            description: req.body.description
+        });
+        category.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            // Successful - redirect to new category record.
+            res.redirect(category.url);
+        });
+    }
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = (req, res) => {
